2f867238a29f048d4c399dd157eb29c31fb86f72
ijal23 <62782190+ijal23@users.noreply.github.com>
2020-04-30 19:18:52 -1200

    Add files via upload

---
+++ src/string/pertemuan03/src/operator/OperatorBinary.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai ke dalam variable
+    //baris 5-7 adalah coment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhammad Rijaldi";
+    int jumlahSks = 24;
+    double indeksPrestasiKumlatif = 3.89;
+
+    public static void main (String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
+
+
+}
