7aef28fdfa7b07c6a9151b0ed973f31ea7ac259b
ijal23 <muhammad.rijaldi23@gmail.com>
2020-05-02 01:59:50 -1200

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -10,5 +10,15 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double indeksPrestasiKumlatif = 3.89;
 
+    public static void main (String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
+
 
 }
