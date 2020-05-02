b4ce8e48d7adae65bc659e583aabb2488f4b3f27
ijal23 <muhammad.rijaldi23@gmail.com>
2020-05-02 02:24:34 -1200

    Operator Equality dan Relational

---
+++ src/operator/OperatorEdanR.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class OperatorEdanR {
+    public static void main(String[] args) {
+        int i = 5;
+        int J= 6;
+        System.out.println(i==5);   //true
+        System.out.println(i==6);   //false
+        System.out.println(i==J);   //false
+        System.out.println(i>J);    //false
+        System.out.println(i<J);    //true
+        System.out.println(5>6);    //false
+        System.out.println(5<6);    //true
+        System.out.println(i!=5);   //false
+        System.out.println(J!=5);   //true
+    }
+}
